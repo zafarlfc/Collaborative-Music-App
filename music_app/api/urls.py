533070e3b50d8dc66432bca18160c18ebc22267c
zafarlfc <zafarlfc@outlook.com>
2024-06-07 22:29:20 +0500

    created api for room model serailizer

--- music_app/api/urls.py
+++ music_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home
+from .views import home, RoomView
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('room', RoomView.as_view())
 ]
--- music_app/api/views.py
+++ music_app/api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-
 from .models import Room
 # Create your views here.
 
