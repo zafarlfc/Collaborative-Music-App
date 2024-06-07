36f2a28ef3b5b2eb1b889fcd87f534faaad6ec55
zafarlfc <zafarlfc@outlook.com>
2024-06-07 22:35:11 +0500

    Fixed api issues

--- music_app/api/views.py
+++ music_app/api/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from rest_framework import generics
 
 from .models import Room
+from .serializers import RoomSerializer
 # Create your views here.
 
 def home(request):
-    return "<h2>My Music App</h2>"
+    return HttpResponse("<h2>My Music App</h2>")
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
