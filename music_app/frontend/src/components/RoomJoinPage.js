bb37f238c31d55b14824330a5986b916e35e5d7e
zafarlfc <zafarlfc@outlook.com>
2024-06-14 23:52:45 +0500

    created join room component

---
+++ music_app/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import { render } from "react-dom"
+
+export default class RoomJoinPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>This is the room join page</p>
+    }
+}
+
+// const appDiv = document.getElementById("app")
+// render(<App/>, appDiv)
