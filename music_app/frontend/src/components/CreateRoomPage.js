ee2da2ddc0f89007efba9495591e0a65f9126797
zafarlfc <zafarlfc@outlook.com>
2024-06-14 23:52:14 +0500

    created create room component

---
+++ music_app/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import { render } from "react-dom"
+
+export default class CreateRoomPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>This is the create room page</p>
+    }
+}
+
+// const appDiv = document.getElementById("app")
+// render(<App/>, appDiv)
