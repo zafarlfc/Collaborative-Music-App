b82b7529df888a55aa6d73fce1c6599508a1f151
zafarlfc <zafarlfc@outlook.com>
2024-07-06 00:52:48 +0500

    room creation issue fixed

--- music_app/api/views.py
+++ music_app/api/views.py
@@ -31,7 +31,7 @@ class CreateRoomView(APIView):
             if queryset.exists():
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
-                room.votes_to_skipv = votes_to_skip
+                room.votes_to_skip = votes_to_skip
                 room.save(update_fields=["guest_can_pause", "votes_to_skip"])
             
             else:
